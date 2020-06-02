28f895f9419b8b4cf6c4bf242b27ce197d670b6b
Jacob Hausmann <jeh7497@gmail.com>
2020-06-02 12:10:39 -0500

    Initial commit

---
+++ FlightAtlasInfrastructure/src/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FlightAtlasInfrastructure
+//
+//  Created by Jacob Hausmann on 6/2/20.
+//  Copyright © 2020 Jacob Hausmann. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
